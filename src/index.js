16155ed14372fb6cf24afadf0b6c554942812229
billyott <willrichott@gmail.com>
2020-12-06 18:16:51 -0500

    done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
+import { createStore } from 'redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(shoppingListItemReducer);
+
+// code change - added Provider to wrap around App
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> /* code change */,
+  document.getElementById('root')
+);
